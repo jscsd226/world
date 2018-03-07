25238695152386eed5fe03f12d2619b768a1e1ec
jscsd226 <305591377@qq.com>
2018-03-07 19:29:20 +0800

    world.java

---
+++ src/world/world.java
@@ -0,0 +1,13 @@
+package world;
+
+public class world {
+
+	public static void main(String[] args)
+	{
+		
+		System.out.println("********************* world ********************");
+		// TODO Auto-generated method stub
+
+	}
+
+}
